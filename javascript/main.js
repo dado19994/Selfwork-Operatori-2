a0e0cc61197ba5c1db3917ac88df6a1e07493dcd
Davide <dignazio90@gmail.com>
2024-08-13 18:10:51 +0200

    Selfwork Operatori 2

---
+++ javascript/main.js
@@ -0,0 +1,48 @@
+// SELFWORK VARIABILI
+
+// let num1 = 17;
+// let num2 = 65;
+
+// console.log("il valore della variabile num1 e' " + num1);
+// console.log(`il valore della variabile num2 e' ${num2}`);
+
+// let stringa = 'Ciao a tutti';
+// stringa = 'Ciao mi chiamo Davide';
+// console.log(`il valore della variabile stringa e' ${stringa}`);
+
+
+// const PIGRECO = 3.14159265359;
+// PIGRECO = 3.14;
+// console.log(`il valore di PIGRECO e' ${PIGRECO}`);
+
+// SELFWORK OPERATORI 1
+
+// let a = 34;
+// let b = 78;
+// let c = 25;
+// let d = 99;
+// let e = 176;
+
+// let somma = a + b + c + d + e;
+// let media = somma / 5;
+// console.log(`la somma tra i numeri equivale a ${somma} e la media equivale a ${media}`);
+
+// SELFWORK OPERATORI 2
+
+let anno_corrente = 2024;
+let anno_di_nascita = 1969;
+
+let difference = anno_corrente - anno_di_nascita;
+console.log(difference);
+
+let tot = 100 - difference;
+console.log(tot);
+
+console.log(`Hai ${difference} anni e ti mancano ${tot} anni per compierne 100`);
+
+
+
+
+
+
+
